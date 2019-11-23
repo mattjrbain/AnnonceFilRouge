db5dde535a68deca005c65514a85e40725e2815b
matt.zaph <zmcbain@icloud.com>
2019-11-23 14:40:56 +0100

    Fix signup validation jQuery

--- src/web/js/validationBis.js
+++ src/web/js/validationBis.js
@@ -1,4 +1,7 @@
-
+let confirmPassword = $("#confirmPassword");
+let confirmAlert = $("#confirmAlert");
+let password = $("#password");
+let mail = $("#mail");
 
 function checkEmpty(selector){
     if ($(selector).val() === ""){
@@ -11,8 +14,19 @@ function checkEmpty(selector){
 }
 
 function samePassword(){
-    if (password.val() !== "" && confirmPassword.val() !== "" && password.val() !== confirmPassword.val()){
+    if (password.val() !== confirmPassword.val()){
         confirmAlert.append("<strong>Les mots de passe ne correspondent pas.</strong>").show();
+        return false;
+    }else return true;
+}
+
+function checkMail(selector) {
+    if (!$(selector).val().match(/[a-zA-Z\d]+@[a-zA-Z\d]+\.[a-z\d]+/)){
+        $(selector).next().append("<strong>Format de mail incorrect</strong>").show();
+        return false;
+    }else {
+        $(selector).next().html("").hide();
+        return true;
     }
 }
 
@@ -22,17 +36,28 @@ $(".toCheck").blur(function () {
 });
 
 // Check same password when leaving password form group
-$(".passGroup").focusout(function () {
+confirmPassword.blur(function () {
     samePassword();
 });
 
+// Check mail format on blur
+mail.blur(function () {
+    checkMail(mail);
+});
+
+// Check when submitting form
 $("#signup").submit(function (event) {
-    //$(".alert").empty();
     $(".toCheck").each(function () {
         if (checkEmpty(this)) {
             event.preventDefault();
         }
     });
+    if (!samePassword()){
+        event.preventDefault();
+    }
+    if (!checkMail(mail)){
+        event.preventDefault();
+    }
 
 });
 
