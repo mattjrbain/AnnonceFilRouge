81522b38b146e1c4a80a4f2d2d3c8432dbb049c6
matt.zaph <zmcbain@icloud.com>
2019-12-01 18:33:50 +0100

    Fix signup validation client side

--- src/web/js/validation.js
+++ src/web/js/validation.js
@@ -1,47 +1,35 @@
-let confirmPassword = $("#confirmPassword");
-let confirmAlert = $("#confirmAlert");
-let password = $("#password");
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        let forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        let validation = Array.prototype.filter.call(forms, function(form) {
 
-function checkMail(selector, event) {
-    if (!$(selector).val().match(/[a-zA-Z\d]+@[a-zA-Z\d]+\.[a-z\d]+/)){
-        $(selector).next().append("<strong>Format de mail incorrect</strong>").show();
-        event.preventDefault();
-    }else {
-        $(selector).next().html("").hide();
-    }
-}
+            let confirmPassword = document.getElementById('confirmPassword');
+            let password = document.getElementById("password");
+            let confirmAlert = $("#confirmAlert");
+            function validatePassword() {
+                if (password.value !== confirmPassword.value) {
+                    confirmPassword.setCustomValidity("Les mots de passe doivent être identiques.");
+                    confirmAlert.text("Les mots de passe doivent être identiques.").show();
+                    event.preventDefault();
+                } else {
+                    confirmPassword.setCustomValidity("");
+                    confirmAlert.text("").hide();
+                }
+            }
 
-// function checkEmpty(selector, event){
-//     if ($(selector).val() === ""){
-//         $(selector).next().append("<strong>Saisie vide</strong><br>").show();
-//         event.preventDefault();
-//         return true;
-//     }else {
-//         $(selector).next().html("").hide();
-//         return false;
-//     }
-// }
+            password.onchange = validatePassword;
+            confirmPassword.onkeyup = validatePassword;
 
-// function samePassword(event){
-//     if (password.val() !== "" && confirmPassword.val() !== "" && password.val() !== confirmPassword.val()){
-//         confirmAlert.append("<strong>Les mots de passe ne correspondent pas.</strong>").show();
-//         event.preventDefault();
-//     }
-// }
-
-confirmPassword.blur(function () {
-    samePassword();
-});
-
-// $(".toCheck").each().blur(function () {
-//     checkEmpty(this);
-// });
-
-$("#signup").submit(function (event) {
-    $(".alert").empty();
-    $(".toCheck").each(function () {
-        checkEmpty(this);
-    });
-    samePassword();
-    checkMail($("#mail"), event);
-});
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
--- src/web/js/validationBis.js
+++ src/web/js/validationBis.js
@@ -3,37 +3,46 @@ let confirmAlert = $("#confirmAlert");
 let password = $("#password");
 let mail = $("#mail");
 
-function checkEmpty(selector){
-    if ($(selector).val() === ""){
-        $(selector).next().html("<strong>Saisie vide</strong><br>").show();
-        return true;
-    }else {
-        $(selector).next().html("").hide();
-        return false;
-    }
-}
+// $(function () {
+//     $('[data-toggle="popover"]').popover();
+// });
+//
+// function checkEmpty(selector){
+//     if ($(selector).val() === ""){
+//         $(selector).next().html("<strong>Saisie vide</strong><br>").show();
+//         return true;
+//     }else {
+//         $(selector).next().html("").hide();
+//         return false;
+//     }
+// }
 
 function samePassword(){
     if (password.val() !== confirmPassword.val()){
-        confirmAlert.append("<strong>Les mots de passe ne correspondent pas.</strong>").show();
-        return false;
-    }else return true;
-}
-
-function checkMail(selector) {
-    if (!$(selector).val().match(/[a-zA-Z\d]+@[a-zA-Z\d]+\.[a-z\d]+/)){
-        $(selector).next().append("<strong>Format de mail incorrect</strong>").show();
+        confirmAlert.text("Les mots de passe doivent être identiques.").show();
+        $("form").checkValidity() === false;
         return false;
     }else {
-        $(selector).next().html("").hide();
+        confirmAlert.text("").hide();
         return true;
     }
 }
 
-// Check empty on blur on all input
-$(".toCheck").blur(function () {
-    checkEmpty(this);
-});
+// function checkMail(selector) {
+//     if (!$(selector).val().match(/[a-zA-Z\d]+@[a-zA-Z\d]+\.[a-z\d]+/)){
+//         $(selector).next().append("<strong>Format de mail incorrect</strong>").show();
+//         $(selector).popover({content: "Format de mail incorrect"});
+//         return false;
+//     }else {
+//         $(selector).next().html("").hide();
+//         return true;
+//     }
+// }
+//
+// // Check empty on blur on all input
+// $(".toCheck").blur(function () {
+//     checkEmpty(this);
+// });
 
 // Check same password when leaving password form group
 confirmPassword.blur(function () {
@@ -41,30 +50,30 @@ confirmPassword.blur(function () {
 });
 
 // Check mail format on blur
-mail.blur(function () {
-    checkMail(mail);
-});
+// mail.blur(function () {
+//     checkMail(mail);
+// });
 
 // Check when submitting form
-$("#signup").submit(function (event) {
-    $(".toCheck").each(function () {
-        if (checkEmpty(this)) {
-            event.preventDefault();
-        }
-    });
-    if (!samePassword()){
-        event.preventDefault();
-    }
-    if (!checkMail(mail)){
-        event.preventDefault();
-    }
-});
-
-$("#addAnnonce").submit(function event() {
-    $(".toCheck").each(function () {
-        if (checkEmpty(this)) {
-            event.preventDefault();
-        }
-    });
-});
+// $("#signup").submit(function (event) {
+//     $(".toCheck").each(function () {
+//         if (checkEmpty(this)) {
+//             event.preventDefault();
+//         }
+//     });
+//     if (!samePassword()){
+//         event.preventDefault();
+//     }
+//     if (!checkMail(mail)){
+//         event.preventDefault();
+//     }
+// });
+//
+// $("#addAnnonce").submit(function event() {
+//     $(".toCheck").each(function () {
+//         if (checkEmpty(this)) {
+//             event.preventDefault();
+//         }
+//     });
+// });
 
